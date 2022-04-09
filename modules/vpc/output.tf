134fd3881569c3c06413333e8df676d9820a61f3
Haggai Lerman <haguyle@gmail.com>
2022-04-09 15:42:18 -0400

    terfform fmt

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -1,7 +1,7 @@
 output "shared_services_public_rt_id" {
-    value = aws_route_table.shared_services_public_rt.id
+  value = aws_route_table.shared_services_public_rt.id
 }
 
 output "shared_services_private_rt_id" {
-    value = aws_route_table.shared_services_private_rt.id
+  value = aws_route_table.shared_services_private_rt.id
 }
--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -1,7 +1,7 @@
 #Create VPC in ca-central-1
 #========================
 resource "aws_vpc" "shared_services_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
