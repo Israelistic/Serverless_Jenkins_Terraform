9cad2af100a859dc0c22889436558c0ce8e8d386
Haggai Lerman <haguyle@gmail.com>
2022-04-09 12:36:34 -0400

    Adding code to main.tf and variable.tf   -main.ts will contain the call to the moudels and provider config  -the  variables.tf contains the variables for the projects

---
+++ main.tf
@@ -0,0 +1,30 @@
+#-----main.tf-----
+#===================
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.9.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = var.region
+}
+
+#Deploy Networking Resources
+#============================
+module "vpc" {
+  source = "./modules/vpc"
+}
+
+#Deploy Compute Resources
+#============================
+module "compute" {
+  source         = "./modules/compute"
+  subnets        = module.vpc.public_subnets
+  security_group = module.vpc.public_sg
+  subnet_ips     = module.vpc.subnet_ips
+}
